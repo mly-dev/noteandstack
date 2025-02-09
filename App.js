dc6c37329656d84a3c179d31f5df5618ac9810bf
mly-dev <Amohamed.kk@ilimi.edu.ne>
2025-02-09 13:33:27 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import AddNoteScreen from "./src/components/NoteForm";
+import NoteDetailScreen from "./src/components/NoteItem";
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#4CAF50",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Notes & Tasks" }}
+        />
+        <Stack.Screen
+          name="AddNote"
+          component={AddNoteScreen}
+          options={{ title: "Nouvelle Note" }}
+        />
+        <Stack.Screen
+          name="NoteDetail"
+          component={NoteDetailScreen}
+          options={{ title: "Détails" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
